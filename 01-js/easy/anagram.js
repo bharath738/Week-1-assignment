0424b631c914b2916ff73f4b2d6888f069f06427
bharath738 <136260581+bharath738@users.noreply.github.com>
2023-06-13 11:30:41 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,19 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.length===str2.length)
+  {    str1=str1.toLowercase();
+       str2=str2.toLowercase();
+       str1=str1.split("").sort().join("");
+       str2=str2.split("").sort().join("");
+       if(str1===str2)
+           {return true;}
+       else
+           {return false;}
+  }
+  else
+  {     return false;
+  }
 }
 
 module.exports = isAnagram;
